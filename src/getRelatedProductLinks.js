e6abae7d6a085e2717e265fbfbcc410dc2bd2e92
Albert Wu <albertywu@gmail.com>
2016-09-03 00:30:35 -0700

    refactor code

--- src/getRelatedProductLinks.js
+++ src/getRelatedProductLinks.js
@@ -5,29 +5,33 @@ const {resolve} = require('url')
 const CAROUSEL_CLASS = 'a-carousel-row-inner'
 
 // (uri: string) => Object
-const options = (uri) => ({
+const getOptions = (uri) => ({
   uri,
   transform: (body) => cheerio.load(body)
 })
 
-// finds all links inside all carousels on the page
-// ($: Cheerio) => Promise<string[]>
-const getProductPaths = ($) => $(`.${CAROUSEL_CLASS}`)
-  .find('a')
-  .toArray()
-  .map(link => $(link).attr('href'))
-  .filter(link => link !== '#')
-
 // (path: string) => string
-const makeAbsoluteUrl = path => resolve(BASE_AMAZON_URL, path)
+const getAbsoluteUrl = path => resolve(BASE_AMAZON_URL, path)
 
 // (paths: string[]) => string[]
-const makeAbsoluteUrls = paths => paths.map(makeAbsoluteUrl)
+const getAbsoluteUrls = paths => paths.map(getAbsoluteUrl)
+
+// finds all links inside all carousels on the page
+// ($: Cheerio) => Promise<string[]>
+function getProductPaths($) {
+  return $(`.${CAROUSEL_CLASS}`)
+    .find('a')
+    .toArray()
+    .map(link => $(link).attr('href'))
+    .filter(link => link !== '#')
+}
 
 // productUrl must be an Amazon product URL!
 // (productUrl: string) => Promise<string[]>
-module.exports = function getRelatedProductLinks(productUrl) {
-  return rp(options(productUrl))
+function getRelatedProductLinks(productUrl) {
+  return rp(getOptions(productUrl))
     .then(getProductPaths)
-    .then(makeAbsoluteUrls)
+    .then(getAbsoluteUrls)
 }
+
+module.exports = getRelatedProductLinks
